51694083f8b1b3331756bd4448acfb3de0cb1c55
Aaron Bikis <aaron.bikis@gmail.com>
2016-01-14 17:49:18 -0800

    third revision

--- TicTacToe/TicTacToe/TicTacToeViewController.m
+++ TicTacToe/TicTacToe/TicTacToeViewController.m
@@ -9,29 +9,110 @@
 #import "TicTacToeViewController.h"
 
 @interface TicTacToeViewController ()
-
+@property (weak, nonatomic) IBOutlet UILabel *whichPlayerLabel;
+@property (strong, nonatomic) IBOutletCollection(UIButton) NSArray *buttonCollection;
+@property NSMutableSet *selectedButtons;
+@property NSMutableArray *buttonNames;
 @end
 
 @implementation TicTacToeViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    self.selectedButtons = [[NSMutableSet alloc] init];
+    self.buttonNames = [[NSMutableArray alloc] initWithObjects:
+                        @"buttonOne",
+                        @"buttonTwo",
+                        @"buttonThree",
+                        @"buttonFour",
+                        @"buttonFive",
+                        @"buttonSix",
+                        @"butonSeven",
+                        @"buttonEight",
+                        @"buttonNine",
+                        nil];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+// User Function
+-(void) userDidPress:(NSNumber*)addObjectWithNumber {
+        //[self.buttonOne setTitle:@"X" forState:UIControlStateNormal]; // add X
+        [self.selectedButtons addObject:addObjectWithNumber]; // add 1 to array
+        //self.buttonOne.enabled =NO; // disable button
+    }
+
+//Computer Functions
+-(BOOL) computersTurn:(NSNumber *)addObjectWithString {
+     NSNumber *randomNumber = [self generateRandomNumber:YES];
+    if([self numberHasBeenPreviouslySelected:self.selectedButtons randomnumber:randomNumber] == NO){
+       // [currentButtonsName setTitle:@"O" forState:UIControlStateNormal];
+        // add clicked button to array
+    }
+    return YES;
+}
+
+                                        
+
+- (UIButton*) buttonNameByNumber:(UInt32)num {
+    UIButton * buttonNumber = [self.buttonNames objectAtIndex:num];
+    return buttonNumber;
+}
+
+
+- (NSNumber *) generateRandomNumber: (BOOL) startGenerating {
+    if (startGenerating == YES){
+        UInt32 randomNumber = arc4random_uniform(8);
+        NSNumber *randomNumberAsNSNumber = [NSNumber numberWithInt:randomNumber];
+        return randomNumberAsNSNumber;
+    }
+    else {
+        return 0;
+    }
+}
+
+
+-(BOOL) numberHasBeenPreviouslySelected:(NSMutableSet *)selectedNumbers randomnumber:(NSNumber*)randomnNumber  {
+    if ([selectedNumbers containsObject:randomnNumber]){
+        return YES;
+    }
+    else {
+        return NO;
+    }
+}
+
+
+
+- (IBAction)onButtonTappedOne:(UIButton *)sender {
+    
+}
+
+
+- (IBAction)onButtonTappedTwo:(UIButton *)sender {
+    
+}
+- (IBAction)onButtonTappedThree:(UIButton *)sender {
+    
+}
+- (IBAction)onButtonTappedFour:(UIButton *)sender {
+}
+- (IBAction)onButtonTappedFive:(UIButton *)sender {
+}
+- (IBAction)onButtonTappedSix:(UIButton *)sender {
+}
+- (IBAction)onButtonTappedSeven:(UIButton *)sender {
+}
+- (IBAction)onButtonTappedEight:(UIButton *)sender {
+}
+- (IBAction)onButtonTappedNine:(UIButton *)sender {
 }
 
 /*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
+ #pragma mark - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+ // Get the new view controller using [segue destinationViewController].
+ // Pass the selected object to the new view controller.
+ }
+ */
 
 @end
